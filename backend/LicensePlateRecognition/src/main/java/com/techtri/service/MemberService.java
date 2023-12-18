f4005d32bdd8e6c4e1b2fe8bed49c176f28242a1
ynnij <ynnnij@gmail.com>
2023-12-18 18:30:18 +0900

    회원가입 구현 및 db 관련 application.properties 수정

---
+++ backend/LicensePlateRecognition/src/main/java/com/techtri/service/MemberService.java
@@ -0,0 +1,32 @@
+package com.techtri.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.techtri.domain.Member;
+import com.techtri.domain.Role;
+import com.techtri.persistence.MemberRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class MemberService {
+	@Autowired
+	private MemberRepository memRepo;
+	PasswordEncoder encoder = new BCryptPasswordEncoder();
+	
+	@Transactional
+	public ResponseEntity<?> memberRegister(Member member) {
+		if(memRepo.findById(member.getId()).isPresent())
+			return ResponseEntity.badRequest().build();
+		
+		member.setRole(Role.ROLE_MEMBER);
+		member.setPassword(encoder.encode(member.getPassword()));
+		memRepo.save(member);
+		
+		return ResponseEntity.ok().build();
+	}
+}
