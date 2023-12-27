1beb27f8a6489d25cc822da1ae62a7e8d2fb5b0f
cjs9615 <38302853+cjs9615@users.noreply.github.com>
2023-12-27 14:12:31 +0900

    날짜시간, 차량번호 csv파일 전처리
    날짜시간, 차량번호 csv파일 전처리

--- data/App.py
+++ data/App.py
@@ -17,6 +17,15 @@ def receive_string():
 	#Spring으로부터 JSON 객체를 전달받음
     dto_json = request.get_json()
 
+    # JSON 응답을 생성
+    response = {
+        'result': '',
+        'image': '',
+        'format': '',  # 이미지 형식에 따라 수정 가능
+        'success': '',
+        'message': ''
+    }
+
     if(dto_json['url'] != '') :
         # 이미지 불러오기
         img = Image.open(requests.get(dto_json['url'], stream=True).raw)
@@ -28,7 +37,9 @@ def receive_string():
     #번호판 이미지 추출
     license_plate_img = model_license_plate(img)
     if(license_plate_img == 'no license_plate') : 
-        return jsonify({'err': 'no license_plate'})
+        response['success'] = 'false'
+        response['message'] = 'no license_plate'
+        return jsonify(response)
     
     image_path = './images/license_plate_img.jpg'
     # 이미지 파일을 Base64로 인코딩
@@ -41,12 +52,10 @@ def receive_string():
     #TrOCR
     generated_text = model_TrOCR()
 
-    # JSON 응답을 생성
-    response = {
-        'result': generated_text,
-        'image': encoded_image,
-        'format': 'jpeg'  # 이미지 형식에 따라 수정 가능
-    }
+    response['result'] = generated_text
+    response['image'] = encoded_image
+    response['format'] = 'jpeg'
+    response['success'] = 'true'
     
     #Spring으로 response 전달
     return jsonify(response)
