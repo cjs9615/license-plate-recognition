3b16d1cb7055a7f076f9db17b1a7647be8bbfd50
maejyomi <141015562+maejyomi@users.noreply.github.com>
2023-12-30 19:59:30 +0900

    MainTable 업데이트

--- frontend/license-plate/src/components/main/MainTable.js
+++ frontend/license-plate/src/components/main/MainTable.js
@@ -1,29 +1,42 @@
+import { LuCopyPlus } from "react-icons/lu";
 
-const MainTable = ({ data }) => {
-    const tbodyItem = data.map((item) => {
+const MainTable = ({ data, setTruckNumber }) => {
+    const selTruck = (seq, plateNumber) => {
+        const selData = [seq, plateNumber];
+        setTruckNumber(selData);
+    }
+    const tbodyItem = data.map((item, idx) => {
+
         return (
-            <tr key={item.seq} className="border-b-2">
-                <td className="py-2">{item.seq}</td>
+            <tr key={item.seq} onClick={() => selTruck(item.seq, item.plateNumber)} className="transition-all border-b even:bg-gray-100 hover:bg-[#ffd3af] cursor-pointer">
+                <td>{idx + 1}</td>
                 <td>{item.plateNumber}</td>
-                <td>{item.regiDate.slice(0,10)}</td>
+                <td>{item.regiDate.slice(0, 10)}</td>
+                <td className="flex justify-center items-center py-3">
+                    <LuCopyPlus className="text-xl text-[#555]" />
+                </td>
             </tr>
 
         )
     })
     return (
-        <div className="w-full">
-            <table className="table-fixed w-full">
-                <thead className="text-left">
-                    <tr>
-                        <th>번호</th>
-                        <th>차량번호</th>
-                        <th>등록일</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {tbodyItem}
-                </tbody>
-            </table>
+        <div className="grow">
+            <h2 className="text-lg font-bold pt-3">DB 검색</h2>
+            <div className="w-full mt-2">
+                <table className="table-auto w-full ">
+                    <thead className="text-center bg-[#EDEDED]">
+                        <tr>
+                            <th className="py-2">번호</th>
+                            <th>차량번호</th>
+                            <th>등록일</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody className="text-center">
+                        {tbodyItem}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
