c430bb4f489890060e9fd7b64d76a0d9341436c4
ynnij <ynnnij@gmail.com>
2024-01-13 16:45:55 +0900

    추론된 번호가 1~3자리인 경우 feature matching 하도록 구현
    - 숫자의 조합으로 DB내 번호를 검색하여 리스트 생성
    - feature matching시 이 리스트 내의 번호를 대상으로 진행

--- backend/LicensePlateRecognition/src/main/java/com/techtri/service/PredictService.java
+++ backend/LicensePlateRecognition/src/main/java/com/techtri/service/PredictService.java
@@ -4,10 +4,14 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import javax.imageio.ImageIO;
 
@@ -36,9 +40,12 @@ import jakarta.transaction.Transactional;
 
 @Service
 public class PredictService {
-	private static final String FLASK_URL = "http://10.125.121.209:5000/receive_image_url";
+	private static final String FLASK_PREDICT_URL = "http://10.125.121.209:5000/receive_image_url";
+	private static final String FLASK_OBJECT_URL = "http://10.125.121.209:5000/receive_number_list";
+
 	private static final int LICENSE_PLATE_FOLDER_INDEX = 1;
 	private static final int OBJECT_IMAGE_FOLDER_INDEX = 2;
+	private static final int NUMBER_LENGTH = 4;
 
 	private PredictRepository predictRepo;
 	private ImagesRepository imagesRepo;
@@ -65,7 +72,7 @@ public class PredictService {
 	@Transactional
 	public PredictResultDto predictLicensePlate(String preImage) throws IOException {
 		RequestFlaskDto requestDto = RequestFlaskDto.builder().url(preImage).build();
-		ResponseFlaskDto responseDto = sendToFlask(requestDto);
+		ResponseFlaskDto responseDto = sendToFlask(requestDto, FLASK_PREDICT_URL);
 
 		boolean isSuccess = responseDto.isSuccess();
 
@@ -87,10 +94,13 @@ public class PredictService {
 
 		String licensePlateImage = decodeAndUploadImage(responseDto.getLicense_plate_image(), responseDto, LICENSE_PLATE_FOLDER_INDEX);
 		String objectImage = decodeAndUploadImage(responseDto.getTruck_image(), responseDto, OBJECT_IMAGE_FOLDER_INDEX);
-
 		saveImage(predict, licensePlateImage, objectImage);
-		
 		String plateNumber = cleanupPlateNumber(responseDto.getResult());
+		// cleanup된 번호판 길이가 1~3자리인 경우 feature matching
+		if(plateNumber.length() < NUMBER_LENGTH)
+			plateNumber = handleNonFourDigitPlateNumber(plateNumber, predict);		
+		
+		// db에서 번호판 검색
 		List<RegisteredCars> numberList = regiCarRepo.findByPlateNumberContainingAndStatus(plateNumber, true);
 
 		return PredictResultDto.builder().licensePlateImage(licensePlateImage)
@@ -119,17 +129,15 @@ public class PredictService {
 		return plateNumber.replaceAll(" ", "");
 	}
 
-	private ResponseFlaskDto sendToFlask(RequestFlaskDto dto) throws JsonProcessingException {
+	private ResponseFlaskDto sendToFlask(Object object, String url) throws JsonProcessingException {
 		RestTemplate restTemplate = new RestTemplate();
 
 		// 헤더를 JSON으로 설정
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 
-		String param = objectMapper.writeValueAsString(dto);
+		String param = objectMapper.writeValueAsString(object);
 		HttpEntity<String> entity = new HttpEntity<String>(param, headers);
-
-		String url = FLASK_URL;
 
 		// response 파싱
 		HttpEntity<String> response = restTemplate.postForEntity(url, entity, String.class);
@@ -159,5 +167,60 @@ public class PredictService {
 		imagesRepo.save(Images.builder().type("license-plate").url(licensePlateImage).predict(predict).build());
 		imagesRepo.save(Images.builder().type("object-image").url(objectImage).predict(predict).build());
 	}
+	
+	private String handleNonFourDigitPlateNumber(String plateNumber, Predict predict) throws JsonProcessingException {
+		List<String> searchStrings = new ArrayList<>(); // DB검색 문자열 리스트
+		List<String> result = new ArrayList<>();
+		
+		int[] arr = {0, 1, 2, 3};
+		boolean[] visited = new boolean[4];
+		findsearchStrings(searchStrings, arr, visited, 0, NUMBER_LENGTH, NUMBER_LENGTH-plateNumber.length(), plateNumber);
+		
+		for(int i=0; i< searchStrings.size() ;i++) 
+			result.addAll(regiCarRepo.findFourDigitLikeQuery(searchStrings.get(i)));
+
+		result = result.stream().distinct().collect(Collectors.toList()); // 중복 제거
+		Map<String, List<String>> map = new HashMap<>();
+		map.put("searchResult", result);
+		
+		ResponseFlaskDto objectResult = sendToFlask(map, FLASK_OBJECT_URL);
+		String predictNumber = objectResult.getResult().substring(0,4);
+		predict.updateNumberAndComment(predictNumber);
+		predictRepo.save(predict);
+		
+		return predictNumber;
+	}
+	
+	private List<Integer> findUnderscoreIndex(int[] arr, boolean[] visited, int n) {
+		List<Integer> underscoreIndex = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			if(visited[i]) {
+				underscoreIndex.add(arr[i]);
+			}
+		}
+		return underscoreIndex;
+	}
+	
+	private void findsearchStrings(List<String> searchStrings,int[] arr, boolean[] visited, int start, int n, int r,String str) {
+	    if(r == 0) {
+	    	List<Integer> underscoreIndex = findUnderscoreIndex(arr, visited, n);
+	    	
+	    	StringBuffer sb = new StringBuffer();
+	    	sb.append(str);
+	    	
+	    	for(int index = 0; index<underscoreIndex.size(); index++) {
+	    		sb.insert(underscoreIndex.get(index), "_");
+	    	}
+	    	searchStrings.add(sb.toString());
+	    	return;
+	    } 
+
+	    for(int i=start; i<n; i++) {
+	        visited[i] = true;
+	        findsearchStrings(searchStrings, arr, visited, i + 1, n, r - 1, str);
+	        visited[i] = false;
+	    }
+	}
+	
 
 }
